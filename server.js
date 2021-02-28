4216012dfbe9346908aaf473de3b05f086e1db5b
Arjun Nayak <arjun430@gmail.com>
2021-03-01 01:52:17 +0530

    fix 1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/angular-on-hero'));
+app.use(express.static(__dirname+'/dist/angular-on-heroku'));
 app.get("/*",function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/angular-on-hero/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/angular-on-heroku/index.html'));
   
 });
 

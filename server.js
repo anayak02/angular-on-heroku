5bdd3e1c58a8f04aebc62d7811d251e2570477df
Arjun Nayak <arjun430@gmail.com>
2021-03-01 01:43:03 +0530

    modified

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname+'/dist/angular-on-hero'));
 app.get("/*",function(req,res){
-  req.sendFile(path.join(__dirname+'/dist/angular-on-hero/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/angular-on-hero/index.html'));
+  
 });
 
 app.listen(process.env.PORT || 8080);

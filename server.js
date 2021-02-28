84e865d9a694d191b08de42b5f66fd69ef62f96f
Arjun Nayak <arjun430@gmail.com>
2021-03-01 01:17:09 +0530

    updated to deploy in heraku1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const exprss = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/angular-on-heroku'));
+app.use(express.static(__dirname+'/dist/angular-on-hero'));
 app.get("/*",function(req,res){
-  req.sendFile(path.join(__dirname+'/dist/angular-on-heroku/index.html'));
+  req.sendFile(path.join(__dirname+'/dist/angular-on-hero/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
